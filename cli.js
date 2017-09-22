d34c77f1d807769fb33d2199865da725484c392a
Vord <32182314+kekvord@users.noreply.github.com>
2017-09-22 01:41:09 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -7,17 +7,23 @@ const table = new Table();
 
 require("yargs")
     .usage("$0 <cmd> [args]")
-    .command("dl [videolink]", "Downloads a video",
+    .command("dl [videolink]", "Downloads a video", {},
         async function (args) {
             const video = await getVideo(args.videolink);
 
             if (!video) return console.log("Cannot get video, please check your link, then try again.");
 
-            table.push(video);
+            table.push({
+                Title: video.title
+            }, {
+                Description: video.description
+            }, {
+                "Video Type": video.video_type
+            }, {
+                Duration: require("humanize-duration")(video.duration * 1000)
+            });
 
             console.log(table.toString());
-
-            const spinner = require("ora")("Downloading...").start();
 
             await download(video.mp4, video.title);
 
