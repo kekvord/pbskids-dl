9f03d374ad247a3404bdb15d1df7680462d34843
Vord <32182314+kekvord@users.noreply.github.com>
2017-09-22 01:41:39 -0500

    Update download.js

--- download.js
+++ download.js
@@ -3,6 +3,13 @@ const fs = require("pn/fs");
 
 module.exports = async(link, fileName) => {
 
-   return request(link).pipe(fs.createWriteStream(`${require("filenamify")(fileName)}.mp4`));
+    const spinner = require("ora")("Downloading...").start();
+
+    const stream = request(link).pipe(fs.createWriteStream(`${require("filenamify")(fileName)}.mp4`));
+
+    stream.on("finish", async() => {
+        spinner.succeed("Done!");
+        process.exit();
+    });
 
 };
